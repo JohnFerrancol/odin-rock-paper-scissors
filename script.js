8b5d9d8358ea087dedb8924ddee3a669a83f41e0
JohnFerrancol <88333166+JohnFerrancol@users.noreply.github.com>
2025-03-16 21:07:23 +0800

    Added playRound function to play a round of Rock, Paper, Scissors

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Created variables to keep track of player's score
+// Keep track of player's score
 let humanScore = 0;
 let computerScore = 0;
 
-// Write a function that randomly returns rock, paper or scissors, this simulate the computer's choice
+// Randomly choose "rock", "paper" or "scissors"
 function getComputerChoice() {
   // randomInteger is used to randomly select a number from 0 - 2
   let randomInteger = Math.floor(Math.random() * 3);
 
-  // Create a switch to map the integers 0 - 2 to Rock, Paper and Scissors
+  // Map the integers 0 - 2 to Rock, Paper and Scissors respectively
   switch (randomInteger) {
     case 0:
       return "rock";
@@ -18,12 +18,66 @@ function getComputerChoice() {
   }
 }
 
-// Write a function that takes in the user's choice of rock, paper or scissors
+// Take in the user's choice of rock, paper or scissors
 function getHumanChoice() {
-  return prompt(
-    `Welcome to Rock Paper scissors!\n Please type 'rock', 'paper', 'scissors'.`
+  let humanChoice = prompt(
+    `Welcome to Rock Paper scissors!\nPlease type 'rock', 'paper',or 'scissors'.`
+  ).toLowerCase(); // Prompt the user for a choice and lowercase it
+
+  // Keep prompting the user for a valid input
+  while (
+    !(
+      humanChoice === "rock" ||
+      humanChoice === "paper" ||
+      humanChoice === "scissors"
+    )
+  ) {
+    alert("Invalid Choice!\nPlease type 'rock', 'paper', or 'scissots'.'");
+    humanChoice = prompt(
+      `Welcome to Rock Paper scissors!\nPlease type 'rock', 'paper',or 'scissors'.`
+    ).toLowerCase();
+  }
+
+  return humanChoice;
+}
+
+// Capitalize strings for display in the console
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+// Play a round of rock paper scissors
+function playRound(humanChoice, computerChoice) {
+  // Get the human's and computer's choices
+  computerChoice = computerChoice;
+  humanChoice = humanChoice; // Remove case sensitivity
+
+  if (humanChoice === computerChoice) {
+    // Display that there is a tie
+    console.log(`Tie! Both picked ${capitalize(humanChoice)}!`);
+  } else if (
+    // Display that the player won and update the player's score
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(
+      `You won! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`
+    );
+    humanScore++;
+  } else {
+    // Display that the computer won and update the computer's score
+    console.log(
+      `You lost! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`
+    );
+    computerScore++;
+  }
+
+  // Display the current score
+  console.log(
+    `Current Score \nPlayer Score = ${humanScore} \nComputer Score = ${computerScore}`
   );
 }
 
-// Testing user input
-console.log(getHumanChoice());
+// Test playing 1 round
+playRound(getHumanChoice(), getComputerChoice());
